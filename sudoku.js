d872dcb173ede1e626e1072eb52c906d9534d273
Zhu Liang <zhuliangg11@gmail.com>
2015-06-28 23:47:06 +0800

    added auto solve functions

--- sudoku.js
+++ sudoku.js
@@ -1,30 +1,49 @@
 /**
  * Created by paradite on 23/2/15.
  */
-var GRID_SIZE = 60;
-var GRIDS_PER_DIMEN = 9;
-var BOX_SIZE = GRID_SIZE * 3;
+var GRID_SIZE = 58;
+var GRIDS_PER_BOX = 3;
+var BOXS_PER_DIMEN = 3;
+var GRIDS_PER_DIMEN = GRIDS_PER_BOX * BOXS_PER_DIMEN;
+var BOX_SIZE = GRID_SIZE * GRIDS_PER_BOX;
 
 var width = GRID_SIZE * GRIDS_PER_DIMEN;
 var height = GRID_SIZE * GRIDS_PER_DIMEN;
 var padding = 12;
 
-var data =
+var locked = false;
+
+var dataString =
     [
-        [0,9,1,2,8,6,5,7,4],
-        [4,8,7,3,5,9,1,2,6],
-        [6,5,2,7,1,4,8,3,9],
+        "630001800",
+        "004200090",
+        "800009067",
+        "956004008",
+        "007306200",
+        "300100746",
+        "790800002",
+        "010007900",
+        "008900073"
+    ];
+
+var data = [GRIDS_PER_DIMEN];
+
+var data0 =
+    [
+        [0,9,1,2,0,0,5,7,4],
+        [4,0,7,3,5,9,1,2,6],
+        [6,5,0,7,1,4,8,3,9],
         //
-        [8,7,5,4,3,1,6,9,2],
+        [0,7,5,4,3,1,6,9,2],
         [2,1,3,9,6,7,4,8,5],
         [9,6,4,0,2,8,7,0,3],
         //
         [1,4,9,6,7,3,2,5,8],
-        [5,3,8,1,0,2,9,6,7],
+        [0,3,8,1,0,2,9,6,7],
         [7,2,6,8,9,5,3,4,1]
     ];
 
-var data_complete =
+var data0_complete =
     [
         [3,9,1,2,8,6,5,7,4],
         [4,8,7,3,5,9,1,2,6],
@@ -45,14 +64,17 @@ var statusCountElement;
 
 function formatData() {
     // add row and column info into each grid for user interaction
-    for (var i = 0; i < data.length; i++) {
-        var row = data[i];
+    for (var i = 0; i < dataString.length; i++) {
+        data[i] = [GRIDS_PER_DIMEN];
+        var row = dataString[i];
+        var digitStrings = row.split("");
         for (var j = 0; j < row.length; j++) {
-            var number = row[j];
+            var digit = parseInt(digitStrings[j]);
             data[i][j] = {
-                number: number,
+                number: digit,
                 row: i,
-                column: j
+                column: j,
+                hint: []
             }
         }
     }
@@ -69,14 +91,70 @@ function getNumber(d) {
     }
 }
 
-function gridClickHandler(d) {
-    log("row: " + d.row + " column:" + d.column + " number: " + d.number);
+function getHint(d) {
+    if(d.hint == null) {
+        // should not happen
+        return "e";
+    } else if(d.hint.length == 0) {
+        return "";
+    } else if(d.hint.length == 1) {
+        return d.hint[0];
+    } else if(d.hint.length < 2) {
+        var numbers = "" + d.hint[0];
+        for (var i = 1; i < d.hint.length; i++) {
+            numbers = numbers + ", " + d.hint[i];
+        }
+        return numbers;
+    } else {
+        return "...";
+    }
+}
+
+function errorSolving() {
+    statusCountElement.text("Error in the grid");
+    updateStatus("This sudoku is impossible to solve");
+    locked = true;
+}
+
+function generateHints() {
+    if(locked) {
+        return;
+    }
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < data[i].length; j++) {
+            var d = data[i][j];
+            generateHint(d);
+        }
+    }
+    updateDOM();
+}
+
+function generateHint(d) {
     if(d.number == 0) {
         var possibleNumbers = getPossibleNumbers(d.row, d.column);
+        log(d.row + " " + d.column + " " + possibleNumbers);
+        if(possibleNumbers.length == 0) {
+            // this should not happen
+            errorSolving();
+        } else {
+            updateDataHints(d.row, d.column, possibleNumbers);
+        }
+    } else {
+        // already solved
+        updateDataHints(d.row, d.column, []);
+    }
+}
 
-        if(possibleNumbers.length == 1) {
+function solveGrid(d) {
+    if(d.number == 0) {
+        var possibleNumbers = d.hint;
+        log(d);
+        if (possibleNumbers.length == 0) {
+            // this should not happen
+            errorSolving();
+        } else if (possibleNumbers.length == 1) {
             updateStatus("The only possible number is " + possibleNumbers[0]);
-            updateData(d.row, d.column, possibleNumbers[0]);
+            updateDataNumber(d.row, d.column, possibleNumbers[0]);
             updateDOM();
         } else {
             var numbers = possibleNumbers[0];
@@ -84,12 +162,18 @@ function gridClickHandler(d) {
                 numbers = numbers + ", " + possibleNumbers[i];
             }
             updateStatus("Possible numbers are " + numbers);
-            //updateData(d.row, d.column, 1);
-            //updateDOM();
         }
     } else {
-        updateStatus("The grid is already completed")
+        updateStatus("The grid is already completed");
     }
+}
+function gridClickHandler(d) {
+    if(locked) {
+        return;
+    }
+    log("row: " + d.row + " column:" + d.column + " number: " + d.number);
+    solveGrid(d);
+    generateHints();
 }
 
 function removeNumber(numbers, n) {
@@ -100,13 +184,22 @@ function removeNumber(numbers, n) {
 }
 function getPossibleNumbers(row, col) {
     var allNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    for (var i = 0; i < GRIDS_PER_DIMEN; i++) {
+    var i, j;
+    for (i = 0; i < GRIDS_PER_DIMEN; i++) {
         var nCol = data[i][col].number;
         var nRow = data[row][i].number;
         // number not possible since it has already appeared
         // remove the number from possible numbers if present
         removeNumber(allNumbers, nCol);
         removeNumber(allNumbers, nRow);
+    }
+    var boxRow = Math.floor(row / BOXS_PER_DIMEN);
+    var boxCol = Math.floor(col / BOXS_PER_DIMEN);
+    for (i = boxRow * GRIDS_PER_BOX; i < (boxRow + 1) * GRIDS_PER_BOX; i++) {
+        for (j = boxCol * GRIDS_PER_BOX; j < (boxCol + 1) * GRIDS_PER_BOX; j++) {
+            var nBox = data[i][j].number;
+            removeNumber(allNumbers, nBox);
+        }
     }
     return allNumbers;
 }
@@ -118,7 +211,6 @@ function updateStatus(text) {
 function updateCount() {
     var count = 0;
     for (var i = 0; i < data.length; i++) {
-        var obj = data[i];
         for (var j = 0; j < data[i].length; j++) {
             var n = data[i][j].number;
             if(n >= 1 && n <= 9) {
@@ -127,7 +219,7 @@ function updateCount() {
                 count++;
             } else {
                 // should not happen
-                statusCountElement.text("This sudoku has become unsolvable");
+                errorSolving();
                 return;
             }
         }
@@ -137,18 +229,27 @@ function updateCount() {
     } else {
         statusCountElement.text(count + " more grids to go");
     }
+    return count;
 }
 
-function updateData(row, col, num) {
+function updateDataNumber(row, col, num) {
     data[row][col].number = num;
+}
+
+function updateDataHints(row, col, hints) {
+    data[row][col].hint = hints;
 }
 
 function initDOM() {
     log("initDOM");
     d3.selectAll(".center")
         .style("width", width + padding + "px");
-    statusTextElement = d3.select(".status-text");
-    statusCountElement = d3.select(".status-count");
+    statusTextElement = d3.select("#status-text");
+    statusCountElement = d3.select("#status-count");
+    autoSolveBtn = d3.select("#auto-solve")
+        .on("click", autoSolve);
+    autoSolveAllBtn = d3.select("#auto-solve-all")
+        .on("click", autoSolveAll);
     var container = d3.select(".container");
 
     var wrapper = container.append("svg")
@@ -185,9 +286,17 @@ function initDOM() {
         .on("click", gridClickHandler);
 
     gridsEnter.append("text")
+        .classed("number", true)
         .attr("dominant-baseline", "middle")
         .attr("x", GRID_SIZE/2)
         .attr("y", GRID_SIZE/2)
+        .attr("pointer-events", "none");
+
+    gridsEnter.append("text")
+        .classed("hint", true)
+        .attr("dominant-baseline", "hanging")
+        .attr("x", GRID_SIZE/6)
+        .attr("y", GRID_SIZE/6)
         .attr("pointer-events", "none");
 
     // overlay 9-grid-box borders
@@ -208,8 +317,11 @@ function initDOM() {
 function updateDOM() {
     log("updateDOM");
 
-    grids.select("text")
+    grids.select(".number")
         .text(getNumber);
+
+    grids.select(".hint")
+        .text(getHint);
 
     grids.select("rect")
         .classed("empty" , function(d) {
@@ -219,9 +331,37 @@ function updateDOM() {
     updateCount();
 }
 
+function eachData(f) {
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < data[i].length; j++) {
+            var d = data[i][j];
+            f(d);
+        }
+    }
+}
+
+function autoSolve() {
+    var count = updateCount();
+    eachData(solveGrid);
+    generateHints();
+    var newCount = updateCount();
+    var solved = count - newCount;
+    updateStatus(solved + " grids solved");
+    return solved;
+}
+
+function autoSolveAll() {
+    var solved = autoSolve();
+    if(solved != 0) {
+        // new state obtained, auto solve again
+        setTimeout(autoSolveAll, 500);
+    }
+}
+
 function log(text) {
     console.log(text);
 }
 
 formatData();
 initDOM();
+generateHints();
